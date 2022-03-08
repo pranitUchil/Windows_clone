11c0a20f1bfc9ed703b5ecd9c2034acc5eff0a9c
pranitUchil <100419556+pranitUchil@users.noreply.github.com>
2022-03-08 15:05:06 +0530

    first commit

---
+++ index.js
@@ -0,0 +1,17 @@
+click.addEventListener('click',()=>{
+
+let startmenu = document.getElementById('startmenu');
+console.log("1")
+if(startmenu.style.display === 'none'){
+
+
+    startmenu.style.display = 'block';
+    
+}else{
+   
+    startmenu.style.display = 'none';
+
+}
+
+
+})
